c16e646f70520d7de8b7016ca719114cbf03192d
Tomas7855 <tomasromero867@gmail.com>
2025-03-22 19:39:36 -0600

    django admin

--- SCC/contabilidad/admin.py
+++ SCC/contabilidad/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CuentaContable, AsientoContable, DetalleAsiento
 
-# Register your models here.
+admin.site.register(CuentaContable)
+admin.site.register(AsientoContable)
+admin.site.register(DetalleAsiento)
